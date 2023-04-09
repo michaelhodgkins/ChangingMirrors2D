0754dd314578d3d8cb2195c75390419bef911d51
michaelhodgkins <mhodgkins1997@gmail.com>
2023-04-09 22:32:16 +0100

    New Shooting Mechanic on Timer
    Coroutine based on bool and fire rate

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
+    int fireRate = 1;
+    
+    public bool hasShot = false;
 
     public GameObject bulletPrefab;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && hasShot == false)
         {
             Shoot();
-       
-            
+            Debug.Log("input");
         }
+    }
+
+    IEnumerator ShootingMechanic()
+    {
+        if (hasShot)
+        {
+            Debug.Log("Shooting mechanic fired off");
+
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            yield return new WaitForSecondsRealtime(fireRate);
+            Debug.Log("wait 1 second");
+
+            hasShot = false;
+        }
+
     }
 
     void Shoot()
     {
-         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        
+        hasShot = true;
+        Debug.Log("has shot == true");
+
+        StartCoroutine(ShootingMechanic());
     }
+
 }
