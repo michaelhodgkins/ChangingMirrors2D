dead2fb6f74528e3c630b2dbedd32898b9c6bf4c
michaelhodgkins <mhodgkins1997@gmail.com>
2023-04-12 16:31:27 +0100

    Changed to Wave System
    - Infinite wave system
    - 10 second wait time between
    - Updated UI for what wave you are on

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,30 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject[] enemy;
     public Transform[] spawnPoints;
-    int spawnLimit = 2;
-    int spawned = 0;
+    [SerializeField] int spawnMax = 4;
+    [SerializeField] int spawnCount = 0;
     public AudioSource AudioSource;
-    
+    public TextMeshProUGUI waveText;
+    [SerializeField] int waveCount = 0;
+
+
     void Start()
     {
         StartCoroutine(WaveSystem());
-
+        waveCount++;
     }
 
     IEnumerator WaveSystem()
     {
-       while(spawned <= spawnLimit)
+       while(spawnCount <= spawnMax)
         {
             Instantiate(enemy[Random.Range(0, enemy.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
-            AudioSource.Play();
             yield return new WaitForSecondsRealtime(2);
-            spawnLimit++;
+            spawnCount++;
         }
+       StartCoroutine(TimeBetweenWaves());
        
+    }
+
+    IEnumerator TimeBetweenWaves()
+    {
+        AudioSource.Play();
+        Debug.Log(TimeBetweenWaves());
+        yield return new WaitForSecondsRealtime(10);
+        spawnCount = 0;
+        spawnMax++;
+        waveCount++;
+        StartCoroutine(WaveSystem());
+    }
+
+    private void Update()
+    {
+        waveText.text = "wave: " + waveCount;
     }
 }
