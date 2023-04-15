9f142f31a7ccc0bb9dfeb10ee74e7e2bf678bb82
michaelhodgkins <mhodgkins1997@gmail.com>
2023-04-15 15:12:57 +0100

    Enemy Health Bars
    - floating health bars above enemy
    - when hit bar decreases

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,24 +5,34 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     public float health = 100;
+    public float maxHealth = 100;
     public float damage = 10;
     public bool hasHit = false;
     public float speed;
     int collisionDmg = 50;
     PlayerController player;
     public float experiencePoints = 10;
+    public GameObject healthBar;
+    public Image healthBarImage;
+
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        health = maxHealth;
     }
 
      void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        healthBar.gameObject.transform.position = this.transform.position;
+    }
+
+    public void UpdateHealthBar()
+    {
+        healthBarImage.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1f);
         
     }
 
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
@@ -36,6 +46,7 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(float damage)
     {
         health -= damage;
+        UpdateHealthBar();
         StartCoroutine(DamageTimer());
         if (health <= 0)
         {
